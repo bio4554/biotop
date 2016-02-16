7485c03e66a50e609df7365f1d71fcc8c6659646
bio4554 <bioshock4554@gmail.com>
2016-02-16 15:32:21 -0600

    Update main.c

--- c/roguelike/main.c
+++ c/roguelike/main.c
@@ -3,19 +3,7 @@
 #include <time.h>
 
 
-#define rand(a,b) ((rand() % b) + a)
 
-#define TESTMAIN
-
-#define MAP_X 100
-#define MAP_Y 50
-#define ROOM_X 10
-#define ROOM_Y 5
-#define MAX_THINGS 20
-#define WALL '#'
-#define FLOOR ' '
-#define FALSE 0
-#define TRUE 0
 
 
 
@@ -28,7 +16,7 @@ void initscreen()
 	noecho();
 }
 
-creature monsters[100];
+
 
 void init_monsters()
 {
