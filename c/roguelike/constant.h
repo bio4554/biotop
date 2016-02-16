5cf8e8e70852110b51ed9703bcd8609007f42bfb
bio4554 <bioshock4554@gmail.com>
2016-02-16 15:32:18 -0600

    Create constant.h

---
+++ c/roguelike/constant.h
@@ -0,0 +1,13 @@
+#define rand(a,b) ((rand() % b) + a)
+
+#define TESTMAIN
+
+#define MAP_X 100
+#define MAP_Y 50
+#define ROOM_X 10
+#define ROOM_Y 5
+#define MAX_THINGS 20
+#define WALL '#'
+#define FLOOR ' '
+#define FALSE 0
+#define TRUE 0
