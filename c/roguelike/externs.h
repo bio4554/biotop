a08f1689211172fdf5be5bb329d499057c642f94
bio4554 <bioshock4554@gmail.com>
2016-02-16 15:41:06 -0600

    Update externs.h

--- c/roguelike/externs.h
+++ c/roguelike/externs.h
@@ -6,3 +6,10 @@ void m_fill(coord, coord, char);
 void m_hall(coord, int, int, char);
 int g_scan(coord, coord, char);
 void g_dungeon(int);
+
+//creature.c
+char getcfd(coord, int);
+int c_move(creature *, int);
+void draw_creatures(creature cre[], int);
+void clear_creatures(creature cre[], int);
+void ai_step(creature *cre[], int);
