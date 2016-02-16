117edc20d9ba2c3a24a376722485caf2ad13778e
bio4554 <bioshock4554@gmail.com>
2016-02-16 15:26:46 -0600

    Update main.c

--- c/roguelike/main.c
+++ c/roguelike/main.c
@@ -17,36 +17,7 @@
 #define FALSE 0
 #define TRUE 0
 
-typedef int bool;
 
-typedef struct coord coord;
-struct coord {
-	int x;
-	int y;
-};
-
-
-typedef struct data data;
-struct data {
-	const char* name;
-	float value;
-	float damage;
-};
-
-typedef struct weapon weapon;
-struct weapon {
-	const char* name;
-	float value;
-	int dice;
-	int sides;
-};
-
-typedef struct creature creature;
-struct creature {
-	coord loc;
-	char icon;
-	bool lit;
-};
 
 void initscreen()
 {
