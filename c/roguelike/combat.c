549e9b45ab04d9edb043fd86a12095b2f59871b0
bio4554 <bioshock4554@gmail.com>
2016-02-16 15:42:29 -0600

    Create combat.c

---
+++ c/roguelike/combat.c
@@ -0,0 +1,19 @@
+int dice(int, int);
+void attack(creature *, creature *);
+
+//Combat stuff
+
+
+int dice(int sides, int times)
+{
+	int i, counter = 0;
+	for(i = 0; i < times; i++)
+	{
+		counter = counter + rand(0, sides);
+	}
+}
+
+void attack(creature *attacker, creature *defender)
+{
+	
+}
