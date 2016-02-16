2dbe74e9d6597881f6905337b479e5fc56eb8e22
bio4554 <bioshock4554@gmail.com>
2016-02-16 15:37:43 -0600

    Update externs.h

--- c/roguelike/externs.h
+++ c/roguelike/externs.h
@@ -1,1 +1,8 @@
-extern creature monsters[MAX_CREATURES];
+
+extern creature monsters[MAX_CREATURES]; //Monsters in the game
+
+//generate.c
+void m_fill(coord, coord, char);
+void m_hall(coord, int, int, char);
+int g_scan(coord, coord, char);
+void g_dungeon(int);
